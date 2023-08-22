9e6d710ebc3e36fa9f11342206dcdba06b663476
B_SSandoo <gksendi@gmail.com>
2023-08-22 17:42:14 +0900

    Release ver 1.0

---
+++ project02-teamB-OUR-consumer/project02-teamB-OUR-consumer/ContentView.swift
@@ -0,0 +1,56 @@
+//
+//  ContentView.swift
+//  project02-teamB-OUR-consumer
+//
+//  Created by 전민돌 on 8/22/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        NavigationStack {
+            TabView {
+                //FeedView 팀에서 넣어주시면 됩니다.
+                Image(systemName: "house.fill")
+                    .tabItem {
+                        Image(systemName: "house.fill")
+                    }
+                //StudyFeedView 팀에서 넣어주시면 됩니다.
+                Image(systemName: "book.fill")
+                    .tabItem {
+                        Image(systemName: "book.fill")
+                    }
+                //RecruitAddView 팀에서 넣어주시면 됩니다.
+                Image(systemName: "plus.app.fill")
+                    .tabItem {
+                        Image(systemName: "plus.app.fill")
+                    }
+                //AlarmView 팀에서 넣어주시면 됩니다.
+                Image(systemName: "bell.fill")
+                    .tabItem {
+                        Image(systemName: "bell.fill")
+                    }
+                //MyPageView 팀에서 넣어주시면 됩니다.
+                Image(systemName: "person.fill")
+                    .tabItem {
+                        Image(systemName: "person.fill")
+                    }
+            }
+            .navigationBarItems(leading: Button(action: {
+                //FeedView로 돌아가기
+            }, label: {
+                Image("OUR_Logo")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+            }))
+        }
+    }
+}
+
+
+struct ContentView_Preview: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
