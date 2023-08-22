77f6e06fec41f09d9bb9251fd4f7a770704d5794
Tag <sea15510@gmail.com>
2023-08-23 01:02:59 +0900

    feat: FeedPhotoAdd Add
    -

---
+++ project02-teamB-OUR-consumer/project02-teamB-OUR-consumer/FeedRecruitPHPicker.swift
@@ -0,0 +1,54 @@
+//
+//  FeedRecruitImagePicker.swift
+//  project02-teamB-OUR-consumer
+//
+//  Created by 윤해수 on 2023/08/22.
+//
+
+import SwiftUI
+import PhotosUI
+
+struct FeedRecruitPHPickerViewControllerWrapper: UIViewControllerRepresentable {
+    @Binding var selectedImages: [UIImage]
+
+    func makeUIViewController(context: Context) -> PHPickerViewController {
+        var configuration = PHPickerConfiguration()
+        configuration.filter = .images
+        configuration.selectionLimit = 0 // 선택가능한 사진의 갯수, 0은 무제한
+
+        let picker = PHPickerViewController(configuration: configuration)
+        picker.delegate = context.coordinator
+        return picker
+    }
+
+    func updateUIViewController(_ uiViewController: PHPickerViewController, context: Context) {}
+
+    func makeCoordinator() -> Coordinator {
+        Coordinator(self)
+    }
+
+    class Coordinator: NSObject, PHPickerViewControllerDelegate {
+        var parent: FeedRecruitPHPickerViewControllerWrapper
+
+        init(_ parent: FeedRecruitPHPickerViewControllerWrapper) {
+            self.parent = parent
+        }
+
+        func picker(_ picker: PHPickerViewController, didFinishPicking results: [PHPickerResult]) {
+            parent.selectedImages = []
+            
+            for result in results {
+                if result.itemProvider.canLoadObject(ofClass: UIImage.self) {
+                    result.itemProvider.loadObject(ofClass: UIImage.self) { (image, error) in
+                        if let image = image as? UIImage {
+                            DispatchQueue.main.async {
+                                self.parent.selectedImages.append(image)
+                            }
+                        }
+                    }
+                }
+            }
+            picker.dismiss(animated: true, completion: nil)
+        }
+    }
+}
