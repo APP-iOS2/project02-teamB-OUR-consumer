15a651db0808b5da187cd29e2d130c61d4539c07
pHH <gudghks56@gmail.com>
2023-08-22 21:15:52 +0900

    feat: Notification Model 추가

--- project02-teamB-OUR-consumer/project02-teamB-OUR-consumer/Model/NotificationItem.swift
+++ project02-teamB-OUR-consumer/project02-teamB-OUR-consumer/Model/NotificationItem.swift
@@ -20,14 +20,6 @@ import Foundation
 
 typealias ID = String
 
-enum NotificationType: String,Codable {
-    case follow
-    case like
-    case reply
-    case studyJoinRequest
-    case studyJoinApproval
-}
-
 struct NotificationItem: Identifiable,Codable{
     let id: ID
     let user: User
@@ -37,30 +29,10 @@ struct NotificationItem: Identifiable,Codable{
     let createdDate: Date // 알림 시간
 }
 
-struct NotificationDTO: Identifiable,Codable{
-    let id: ID
-    let userId: ID
-    let type: NotificationType
-    let content: String
-    let isRead: Bool
-    let createdDate: Date // 알림 시간
-}
 
 
 //TODO: - TestModel 추후 변경
 struct User: Codable{
     let id: ID
     let name: String
-}
-
-
-// 알림 항목 모델
-struct NotificationItem2 {
-    enum NotificationType {
-        case follow, like, studyJoinRequest, studyJoinApproval
-    }
-
-    let type: NotificationType
-    let text: String
-    let date: Date // 알림 시간
 }
