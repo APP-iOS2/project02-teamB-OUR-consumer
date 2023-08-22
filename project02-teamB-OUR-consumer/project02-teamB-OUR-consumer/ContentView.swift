2cf2093ae62d139625eb112d392634977470e5a5
a-jb97 <a_jb97@naver.com>
2023-08-22 10:46:46 +0900

    consumer 프로젝트 생성

---
+++ project02-teamB-OUR-consumer/project02-teamB-OUR-consumer/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  project02-teamB-OUR-consumer
+//
+//  Created by 전민돌 on 8/22/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ project02-teamB-OUR-consumer/project02-teamB-OUR-consumer/project02_teamB_OUR_consumerApp.swift
@@ -0,0 +1,17 @@
+//
+//  project02_teamB_OUR_consumerApp.swift
+//  project02-teamB-OUR-consumer
+//
+//  Created by 전민돌 on 8/22/23.
+//
+
+import SwiftUI
+
+@main
+struct project02_teamB_OUR_consumerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
