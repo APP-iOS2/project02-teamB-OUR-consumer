b3b1538dad386f1aeac444e8a97a946a5c4d70e7
YeoSungeun <dutjddms@naver.com>
2023-08-22 15:48:55 +0900

    add : StudyReplyView 추가

---
+++ project02-teamB-OUR-consumer/project02-teamB-OUR-consumer/StudyReplyView.swift
@@ -0,0 +1,20 @@
+//
+//  StudyReplyView.swift
+//  project02-teamB-OUR-consumer
+//
+//  Created by 여성은 on 2023/08/22.
+//
+
+import SwiftUI
+
+struct StudyReplyView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct StudyReplyView_Previews: PreviewProvider {
+    static var previews: some View {
+        StudyReplyView()
+    }
+}
