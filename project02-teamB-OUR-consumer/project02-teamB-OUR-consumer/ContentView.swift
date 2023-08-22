e2216753828fd10a26378a874001d2a467e483af
Tag <sea15510@gmail.com>
2023-08-22 13:19:04 +0900

    feat: NavigationTitle 추가
    <body> 생략 가능
    
    -

--- project02-teamB-OUR-consumer/project02-teamB-OUR-consumer/ContentView.swift
+++ project02-teamB-OUR-consumer/project02-teamB-OUR-consumer/ContentView.swift
@@ -9,32 +9,42 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        TabView {
-            //FeedView 팀에서 넣어주시면 됩니다.
-            Image(systemName: "house.fill")
-                .tabItem {
-                    Image(systemName: "house.fill")
-                }
-            //StudyFeedView 팀에서 넣어주시면 됩니다.
-            Image(systemName: "book.fill")
-                .tabItem {
-                    Image(systemName: "book.fill")
-                }
-            //RecruitAddView 팀에서 넣어주시면 됩니다.
-            Image(systemName: "plus.app.fill")
-                .tabItem {
-                    Image(systemName: "plus.app.fill")
-                }
-            //AlarmView 팀에서 넣어주시면 됩니다.
-            Image(systemName: "bell.fill")
-                .tabItem {
-                    Image(systemName: "bell.fill")
-                }
-            //MyPageView 팀에서 넣어주시면 됩니다.
-            Image(systemName: "person.fill")
-                .tabItem {
-                    Image(systemName: "person.fill")
-                }
+        NavigationStack {
+            TabView {
+                //FeedView 팀에서 넣어주시면 됩니다.
+                Image(systemName: "house.fill")
+                    .tabItem {
+                        Image(systemName: "house.fill")
+                    }
+                //StudyFeedView 팀에서 넣어주시면 됩니다.
+                Image(systemName: "book.fill")
+                    .tabItem {
+                        Image(systemName: "book.fill")
+                    }
+                //RecruitAddView 팀에서 넣어주시면 됩니다.
+                Image(systemName: "plus.app.fill")
+                    .tabItem {
+                        Image(systemName: "plus.app.fill")
+                    }
+                //AlarmView 팀에서 넣어주시면 됩니다.
+                Image(systemName: "bell.fill")
+                    .tabItem {
+                        Image(systemName: "bell.fill")
+                    }
+                //MyPageView 팀에서 넣어주시면 됩니다.
+                Image(systemName: "person.fill")
+                    .tabItem {
+                        Image(systemName: "person.fill")
+                    }
+            }
+            .navigationTitle("")
+            .navigationBarItems(leading: Button(action: {
+                
+            }, label: {
+                Image("OUR_Logo")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+            }))
         }
     }
 }
