b17e4ae552dfe680925dc44d94ac283d85b4a14e
Tag <sea15510@gmail.com>
2023-08-22 11:53:33 +0900

    TabView Add
    -

--- project02-teamB-OUR-consumer/project02-teamB-OUR-consumer/ContentView.swift
+++ project02-teamB-OUR-consumer/project02-teamB-OUR-consumer/ContentView.swift
@@ -9,16 +9,38 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        TabView {
+            //FeedView 팀에서 넣어주시면 됩니다.
+            Image(systemName: "house.fill")
+                .tabItem {
+                    Image(systemName: "house.fill")
+                }
+            //StudyFeedView 팀에서 넣어주시면 됩니다.
+            Image(systemName: "book.fill")
+                .tabItem {
+                    Image(systemName: "book.fill")
+                }
+            //RecruitAddView 팀에서 넣어주시면 됩니다.
+            Image(systemName: "plus.app.fill")
+                .tabItem {
+                    Image(systemName: "plus.app.fill")
+                }
+            //AlarmView 팀에서 넣어주시면 됩니다.
+            Image(systemName: "bell.fill")
+                .tabItem {
+                    Image(systemName: "bell.fill")
+                }
+            //MyPageView 팀에서 넣어주시면 됩니다.
+            Image(systemName: "person.fill")
+                .tabItem {
+                    Image(systemName: "person.fill")
+                }
         }
-        .padding()
     }
 }
 
-#Preview {
-    ContentView()
+struct ContentView_Previews: PreviewProvider {
+  static var previews: some View {
+      ContentView()
+  }
 }
