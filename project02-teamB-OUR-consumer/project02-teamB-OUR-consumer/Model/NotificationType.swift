79ada4b118046a50e4a37e82f9437b367dff8292
pHH <gudghks56@gmail.com>
2023-08-22 21:16:18 +0900

    feat: Notification Type 추가

--- project02-teamB-OUR-consumer/project02-teamB-OUR-consumer/Model/NotificationType.swift
+++ project02-teamB-OUR-consumer/project02-teamB-OUR-consumer/Model/NotificationType.swift
@@ -6,3 +6,30 @@
 //
 
 import Foundation
+
+
+enum NotificationType: String,Codable {
+    case follow
+    case like
+    case reply
+    case studyJoinRequest
+    case studyJoinApproval
+    case none
+    
+    func getAccessLevel() -> Access{
+        switch self {
+        case .follow,.like,.reply:
+            return .public
+        case .studyJoinRequest,.studyJoinApproval:
+            return .personal
+        case .none:
+            return .none
+        }
+    }
+    
+    enum Access{
+        case `public`
+        case `personal`
+        case none
+    }
+}
