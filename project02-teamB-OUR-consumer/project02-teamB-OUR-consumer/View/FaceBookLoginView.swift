b1b562a05a698807419d09533b5b88934f8cd973
SonYoonho <yoonhoson@naver.com>
2023-08-23 10:39:15 +0900

    feat: facebook-signup

---
+++ project02-teamB-OUR-consumer/project02-teamB-OUR-consumer/View/FaceBookLoginView.swift
@@ -0,0 +1,20 @@
+//
+//  FaceBookLoginView.swift
+//  project02-teamB-OUR-consumer
+//
+//  Created by SONYOONHO on 2023/08/23.
+//
+
+import SwiftUI
+
+struct FaceBookLoginView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct FaceBookLoginView_Previews: PreviewProvider {
+    static var previews: some View {
+        FaceBookLoginView()
+    }
+}
