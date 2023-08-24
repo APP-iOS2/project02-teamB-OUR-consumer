eec5c518e226fda2dd5b2be450f5e841472e9731
ddudi <ddudios@naver.com>
2023-08-24 14:04:50 +0900

    design: navigation title, change tab bar name  <타입>: <제목>

--- project02-teamB-OUR-consumer/project02-teamB-OUR-consumer/Views/AlarmView/CustomTabView.swift
+++ project02-teamB-OUR-consumer/project02-teamB-OUR-consumer/Views/AlarmView/CustomTabView.swift
@@ -15,8 +15,8 @@ struct CustomTabView: View {
 
     var body: some View {
         HStack(spacing: 0) {
-            CustomTabButton(selectedTab: $selectedTab, text: "깔깔", index: 0)
-            CustomTabButton(selectedTab: $selectedTab, text: "근엄", index: 1)
+            CustomTabButton(selectedTab: $selectedTab, text: "활동 알림", index: 0)
+            CustomTabButton(selectedTab: $selectedTab, text: "스터디 알림", index: 1)
         }
         .padding(.horizontal)
         .font(.system(size: 16, weight: .semibold))
