ef75d94f6a12a338174c63d8215582c5f40068cd
B_SSandoo <gksendi@gmail.com>
2023-08-25 14:39:39 +0900

    feat: TitleView의 textfield 애니메이션 추가
    TitleView 내 상단 마진과
    ZStack 위치 임시 Padding으로 해결
    
    이후 수정 필요

--- project02-teamB-OUR-consumer/project02-teamB-OUR-consumer/TitleView.swift
+++ project02-teamB-OUR-consumer/project02-teamB-OUR-consumer/TitleView.swift
@@ -10,30 +10,89 @@ import SwiftUI
 struct TitleView: View {
     
     @State private var searchFeed: String = ""
+    @State var isAnimation: Bool = false
+    @State private var name = ""
+    @State private var isEditingName = false
+    @State var searchCategory: [String] = ["게시글", "스터디", "사용자"]
+    @State var selectedCategory: String = "게시글"
+    @State var isFocus: Bool = false
     
     var body: some View {
-        VStack {
+        ZStack {
             HStack {
                 Image("OUR_Logo_02")
                     .resizable()
                     .aspectRatio(contentMode: .fit)
                     .frame(width: 100)
                     .padding(EdgeInsets(top: 0, leading: 20, bottom: 0, trailing: 0))
-                
                 Spacer()
-
             }
-            
-            HStack {
-                Image(systemName: "magnifyingglass")
-                    .foregroundColor(.gray)
-                TextField("오늘의 피드를 검색해보세요.", text: $searchFeed, axis: .horizontal)
+            ZStack {
+                Rectangle()
+                    .foregroundColor(isAnimation ? Color.gray.opacity(0.3) : Color.gray.opacity(0.2))
+                    .frame(width: 370, height: isAnimation ? 100 : 50)
+                    .cornerRadius(20)
+                    .offset(y: isAnimation ? 0 : -25)
+                    .animation(.spring(response: 0.5, blendDuration: 1))
+                    .padding(.top, 150)
+                HStack {
+                    Image(systemName: "magnifyingglass")
+                        .foregroundColor(.black)
+                        .padding(.leading, 30)
+                    
+                    TextField("오늘의 \(selectedCategory)를 검색해보세요.", text: $searchFeed, onEditingChanged: { editing in
+                        isFocus = editing
+                        if editing {
+                            isAnimation = true
+                        } else {
+                            isAnimation = false
+                        }
+                    })
+                    .overlay (
+                        Text("")
+                            .padding(.leading, -100)
+                            .foregroundColor(.black)
+                    )
+                    Button {
+                        UIApplication.shared.endEditing()
+                        
+                    } label: {
+                        Text("확인")
+                            .foregroundColor(.black)
+                            .padding(.trailing, 10)
+                    }
+                    
+                    Spacer()
+                }
+                .padding(.top, 100)
+                
             }
-            .frame(width: 360, height: 50, alignment: .center)
-            .background(Color.gray.opacity(0.2))
-            .cornerRadius(20)
-            .padding(EdgeInsets(top: 0, leading: 20, bottom: 0, trailing: 20))
+            .onTapGesture {
+                UIApplication.shared.endEditing()
+            }
+            Picker("Food Category", selection: $selectedCategory) {
+                ForEach(searchCategory, id: \.self) { category in
+                    Text(category)
+                }
+            }
+            .opacity(isAnimation ? 1.0 : 0.0)
+            .animation(.easeIn(duration: 0.5))
+            .frame(width: 350)
+            .pickerStyle(SegmentedPickerStyle())
+            .padding(.top, 200)
+            Spacer()
         }
+    }
+}
+
+func isFocusSign(isFocus: Bool) -> String {
+    return isFocus ? "O" : "X"
+}
+
+
+extension UIApplication {
+    func endEditing() {
+        sendAction(#selector(UIResponder.resignFirstResponder), to: nil, from: nil, for: nil)
     }
 }
 
