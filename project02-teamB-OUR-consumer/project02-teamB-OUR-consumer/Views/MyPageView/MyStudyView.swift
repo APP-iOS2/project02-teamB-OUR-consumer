635744d25dc5e6bba8e7ac8f746c8aad53f25933
Bongjooheon <112856133+Bongjooheon@users.noreply.github.com>
2023-08-25 09:57:47 +0900

    MyPageSttudyView 수정

--- project02-teamB-OUR-consumer/project02-teamB-OUR-consumer/Views/MyPageView/MyStudyView.swift
+++ project02-teamB-OUR-consumer/project02-teamB-OUR-consumer/Views/MyPageView/MyStudyView.swift
@@ -13,81 +13,101 @@ struct MyStudyView: View {
     
     var body: some View {
         NavigationStack {
-            ScrollView(.horizontal, showsIndicators: false) {
-                HStack(spacing: 20) {
-                ForEach(studyStore.studyArray) { study in
-                        ZStack {
-                            RoundedRectangle(cornerRadius: 10)
-                                .frame(width: 160, height: 300)
-                                .overlay(
-                                    RoundedRectangle(cornerRadius: 10)
-                                        .stroke(Color.gray, lineWidth: 1)
-                                )
-                                .foregroundColor(.white)
-                                .padding(2)
-                                .shadow(radius: 3)
-
-                            VStack {
-                                AsyncImage(url: study.imageURL) { image in
-                                    image
-                                        .resizable()
-                                        .aspectRatio(contentMode: .fill)
-                                        .frame(width: 130, height: 130)
-                                        .cornerRadius(10)
-                                } placeholder: {
-                                    ProgressView()
-                                }
-                                VStack(alignment: .leading) {
-                                    Text("\(study.title)")
-                                        .frame(width: 130, height: 50)
-                                        .bold()
-                                }
-                                .fixedSize(horizontal: false, vertical: true)
-                                .frame(width: 100)
-                                VStack(alignment: .leading) {
-                                    HStack {
-                                        Image(systemName: "calendar.badge.clock")
+            ScrollView {
+                ScrollView(.horizontal, showsIndicators: false) {
+                    HStack(spacing: 20) {
+                        ForEach(studyStore.studyArray) { study in
+                            ZStack {
+                                RoundedRectangle(cornerRadius: 10)
+                                    .frame(width: 160, height: 300)
+                                    .overlay(
+                                        RoundedRectangle(cornerRadius: 10)
+                                            .stroke(Color.gray, lineWidth: 1)
+                                    )
+                                    .foregroundColor(.white)
+                                    .padding(2)
+                                    .shadow(radius: 3)
+                                
+                                VStack {
+                                    AsyncImage(url: study.imageURL) { image in
+                                        image
                                             .resizable()
-                                            .aspectRatio(contentMode: .fit)
-                                            .frame(width: 15, height: 15)
-                                        VStack {
+                                            .aspectRatio(contentMode: .fill)
+                                            .frame(width: 130, height: 130)
+                                            .cornerRadius(10)
+                                    } placeholder: {
+                                        ProgressView()
+                                    }
+                                    VStack(alignment: .leading) {
+                                        Text("\(study.title)")
+                                            .frame(width: 130, height: 50)
+                                            .bold()
+                                    }
+                                    .fixedSize(horizontal: false, vertical: true)
+                                    .frame(width: 100)
+                                    VStack(alignment: .leading) {
+                                        HStack {
+                                            Image(systemName: "calendar.badge.clock")
+                                                .resizable()
+                                                .aspectRatio(contentMode: .fit)
+                                                .frame(width: 15, height: 15)
                                             VStack {
-                                                Text("\(study.date)")
-                                                    .frame(width: 110)
+                                                VStack {
+                                                    Text("\(study.date)")
+                                                        .frame(width: 110)
                                                         .lineLimit(1) //3줄까지만 제한을 둔다. ()안에 nil을 쓰면 무제한
+                                                }
+                                                .fixedSize(horizontal: false, vertical: true)
                                             }
-                                            .fixedSize(horizontal: false, vertical: true)
                                         }
+                                        .font(.footnote)
+                                        .foregroundColor(.gray)
+                                        HStack{
+                                            Image(systemName: "person.3.sequence.fill")
+                                                .resizable()
+                                                .aspectRatio(contentMode: .fit)
+                                                .frame(width: 19, height: 15)
+                                            Text("\(study.currentMemberCount)/\(study.totalMemberCount)")
+                                        }
+                                        .font(.footnote)
+                                        .foregroundColor(.gray)
                                     }
-                                    .font(.footnote)
-                                    .foregroundColor(.gray)
-                                    HStack{
-                                        Image(systemName: "person.3.sequence.fill")
-                                            .resizable()
-                                            .aspectRatio(contentMode: .fit)
-                                            .frame(width: 19, height: 15)
-                                        Text("\(study.currentMemberCount)/\(study.totalMemberCount)")
+                                    NavigationLink {
+                                        StudyDetailView()
+                                    } label: {
+                                        Text("자세히 보기")
+                                            .font(.system(size: 15))
+                                            .frame(width: 100, height: 0)
+                                            .bold()
+                                            .foregroundColor(.white)
+                                            .padding()
+                                            .background(mainColor)
+                                            .cornerRadius(10)
                                     }
-                                    .font(.footnote)
-                                    .foregroundColor(.gray)
-                                }
-                                NavigationLink {
-                                    StudyDetailView()
-                                } label: {
-                                    Text("자세히 보기")
-                                        .font(.system(size: 15))
-                                        .frame(width: 100, height: 0)
-                                        .bold()
-                                        .foregroundColor(.white)
-                                        .padding()
-                                        .background(mainColor)
-                                        .cornerRadius(10)
                                 }
                             }
                         }
                     }
+                    .padding(.leading)
                 }
-                .padding(.leading)
+                Rectangle()
+                    .fill(Color("DefaultGray"))
+                VStack(alignment: .leading) {
+                    HStack {
+                        Text("마감된 스터디")
+                            .font(.system(size: 16))
+                            .bold()
+                        Spacer()
+                    }
+                    Divider()
+                    .padding(.vertical, 5)
+                    Text("아직 마감된 스터디가 없습니다.")
+                }
+                .padding(.top, 11)
+                .padding(.horizontal)
+                .foregroundColor(.black)
+                Rectangle()
+                    .fill(Color("DefaultGray"))
             }
         }
     }
