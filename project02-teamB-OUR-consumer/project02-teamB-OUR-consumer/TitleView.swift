c1b5c59b4ec282c84f2fd570b34068b028b11a5f
B_SSandoo <gksendi@gmail.com>
2023-08-25 10:35:39 +0900

    feat: Logo 수정

--- project02-teamB-OUR-consumer/project02-teamB-OUR-consumer/TitleView.swift
+++ project02-teamB-OUR-consumer/project02-teamB-OUR-consumer/TitleView.swift
@@ -14,26 +14,14 @@ struct TitleView: View {
     var body: some View {
         VStack {
             HStack {
-                Image("Logo")
+                Image("OUR_Logo_02")
                     .resizable()
                     .aspectRatio(contentMode: .fit)
-                    .frame(width: 50)
+                    .frame(width: 100)
                     .padding(EdgeInsets(top: 0, leading: 20, bottom: 0, trailing: 0))
-                Text("OUR")
-                    .font(.system(size: 25))
-                    .fontWeight(.black)
-                    .padding(EdgeInsets(top: 22, leading: 0, bottom: 0, trailing: 0))
                 
                 Spacer()
-                
-                Button {
-                    //
-                } label: {
-                    Image(systemName: "gearshape.fill")
-                        .font(.system(size: 25))
-                        .padding(EdgeInsets(top: 20, leading: 0, bottom: 0, trailing: 20))
-                        .tint(.black)
-                }
+
             }
             
             HStack {
